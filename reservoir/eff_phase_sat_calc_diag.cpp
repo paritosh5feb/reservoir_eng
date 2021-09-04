fe7b4c258d52c2087cb1456b2151ac5e3c37d293
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:43:30 +0530

    Implementation of the member functions

--- reservoir/eff_phase_sat_calc_diag.cpp
+++ reservoir/eff_phase_sat_calc_diag.cpp
@@ -15,3 +15,36 @@ eff_phase_sat_calc_diag::~eff_phase_sat_calc_diag()
     delete ui;
 }
 
+/* Implementation of all the member functions in the class defined in the header file */
+void eff_phase_sat_calc_diag::on_effective_phase_saturation_sog_calc_button_clicked()
+{
+    double sog;
+    double swc;
+    double output;
+    bool ok = false;
+    QString out;
+    sog = ui->t1_s_og->toPlainText().toDouble(&ok);
+    swc = ui->t2_s_wc->toPlainText().toDouble(&ok);
+    output = sog/(1-swc);
+    out = QString::number(output);
+    ui->output->setText(out);
+}
+
+void eff_phase_sat_calc_diag::on_effective_phase_saturation_sw_calc_button_clicked()
+{
+    double swc;
+    double sw;
+    double output;
+    bool ok = false;
+    QString out;
+    sw = ui->t3_s_w->toPlainText().toDouble(&ok);
+    swc = ui->t4_s_wc->toPlainText().toDouble(&ok);
+    output = (sw-swc)/(1-swc);
+    out = QString::number(output);
+    ui->output->setText(out);
+}
+
+void eff_phase_sat_calc_diag::on_close_clicked()
+{
+    eff_phase_sat_calc_diag::close();
+}

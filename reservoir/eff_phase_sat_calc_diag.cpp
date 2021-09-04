a0be72b076f25bf9bddeaecb71f634d2c5b3b6cf
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:41:55 +0530

    added template code

--- reservoir/eff_phase_sat_calc_diag.cpp
+++ reservoir/eff_phase_sat_calc_diag.cpp
@@ -1,1 +1,17 @@
+/* Include the header file containing the class and member function definitions */
+#include "eff_phase_sat_calc_diag.h"
+
+
+/* Template code */
+eff_phase_sat_calc_diag::eff_phase_sat_calc_diag(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::eff_phase_sat_calc_diag)
+{
+    ui->setupUi(this);
+}
+
+eff_phase_sat_calc_diag::~eff_phase_sat_calc_diag()
+{
+    delete ui;
+}
 

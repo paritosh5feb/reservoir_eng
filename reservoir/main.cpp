fee641e52acccb313a889bd29bc5cb239c27abba
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:09:57 +0530

    wrote parent calling code for the Executables

--- reservoir/main.cpp
+++ reservoir/main.cpp
@@ -1,1 +1,14 @@
+/* Include the header file the primary executable code (c++) - by 'mainwindow.cpp' 
+ * Include the QT Application header
+*/
+#include "mainwindow.h"
+#include <QApplication> 
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
 

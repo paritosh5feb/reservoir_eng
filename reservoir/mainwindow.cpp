dfb0cb76a1818d5baa5470e59c51270f99f97a1c
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:27:05 +0530

    Implementation of the member functions

--- reservoir/mainwindow.cpp
+++ reservoir/mainwindow.cpp
@@ -12,3 +12,16 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+/*
+ * Implementation of the member functions defined in mainwindow.h
+*/
+void MainWindow::on_rb1_effective_phase_calcs_clicked()
+{
+    per_calcs = 1;
+}
+
+void MainWindow::on_rb2_2_phase_rel_per_calcs_clicked()
+{
+    per_calcs = 2;
+}

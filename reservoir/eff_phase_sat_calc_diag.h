7c9b4842a49adc130dd98d040d9e433628deaa20
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:31:06 +0530

    Update eff_phase_sat_calc_diag.h

--- reservoir/eff_phase_sat_calc_diag.h
+++ reservoir/eff_phase_sat_calc_diag.h
@@ -1,1 +1,13 @@
+#ifndef EFF_PHASE_SAT_CALC_DIAG_H
+#define EFF_PHASE_SAT_CALC_DIAG_H
+
+#include <QDialog>
+
+namespace Ui {
+class eff_phase_sat_calc_diag;
+}
+
+
+/* This file contains the class definition of the dialog box for effective phase saturation calculations.
+*/
 

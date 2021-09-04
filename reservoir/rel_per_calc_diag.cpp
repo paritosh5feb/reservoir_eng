b849167e15f9295e7f220b0757a459236d49a38e
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:49:55 +0530

    Added header files template code

--- reservoir/rel_per_calc_diag.cpp
+++ reservoir/rel_per_calc_diag.cpp
@@ -1,1 +1,17 @@
+#include "rel_per_calc_diag.h"
+#include <cmath>
+#include <QItemDelegate>
+#include <QObject>
+#include <QModelIndex>
+rel_per_calc_diag::rel_per_calc_diag(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::rel_per_calc_diag)
+{
+    ui->setupUi(this);
 
+}
+
+rel_per_calc_diag::~rel_per_calc_diag()
+{
+    delete ui;
+}

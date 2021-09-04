9b93847f08c15fd14809c68a42b5ba0fe0592c1a
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-09-04 16:15:01 +0530

    Parent Class Definition and member functions

--- reservoir/mainwindow.h
+++ reservoir/mainwindow.h
@@ -6,3 +6,28 @@
 #include <QDialog>
 #include <QtGui>
 #include <cmath>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_rb1_effective_phase_calcs_clicked();
+
+    void on_rb2_2_phase_rel_per_calcs_clicked();
+
+    void on_proceed_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    int per_calcs;
+};
+#endif // MAINWINDOW_H
